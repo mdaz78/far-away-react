b1f14b368ebbeb5838c9f18b0a21cb288318a957
mdabuzafar <mdabuzafar78@gmail.com>
2025-11-21 12:48:35 +0530

    feat: restructure application by moving App component to a new file and updating main entry point

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
-import './App.css';
-import Form from './components/Form';
-import Logo from './components/Logo';
-import PackingList from './components/PackingList';
-import Stats from './components/Stats';
+import Form from './Form';
+import Logo from './Logo';
+import PackingList from './PackingList';
+import Stats from './Stats';
 
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
